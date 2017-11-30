8d8b12595ee05785589f6eb704fa0775618b7d2a
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-11-30 21:47:03 +0100

    zadanie 9.1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function getTriangleArea(a, h){
+	if(a>0 && h>0){
+		return a*h/2;
+	} else{
+		return 'Nieprawidłowe dane'
+	}
+}
+
+console.log(getTriangleArea(10,6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(6, 8);
+var triangle3Area = getTriangleArea(12, 18);
